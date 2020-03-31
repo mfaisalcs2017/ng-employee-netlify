d57c54cec8c1ed552be13699a3d3cd636405e721
mfaisalcs2017 <46646996+mfaisalcs2017@users.noreply.github.com>
2020-03-31 08:01:15 +0530

    first-commit

---
+++ src/app/shared/employee.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import "rxjs/Rx";
+
+import { Employee } from "./employee.model";
+
+@Injectable({
+  providedIn: "root"
+})
+export class EmployeeService {
+  selectedEmployee: Employee;
+  employees: Employee[];
+  // readonly baseURL = 'http://localhost:3000/employees';
+  readonly baseURL = "https://springoose.herokuapp.com/employees";
+  constructor(private http: HttpClient) {}
+
+  postEmployee(emp: Employee) {
+    return this.http.post(this.baseURL, emp);
+  }
+
+  getEmployeeList() {
+    return this.http.get(this.baseURL);
+  }
+
+  putEmployee(emp: Employee) {
+    return this.http.put(this.baseURL + `/${emp.id}`, emp);
+  }
+
+  deleteEmployee(id: string) {
+    return this.http.delete(this.baseURL + `/${id}`);
+  }
+}
